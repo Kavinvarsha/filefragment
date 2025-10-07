5c4dc1de00849b4232b8a5e048bc9a107a5dfab7
varshaa <kavinvarsha33@gmail.com>
2025-10-07 15:52:21 +0530

    getting input text

---
+++ FileFragmentationMVC/TextView.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TextFragmentationMVC
+{
+    class TextView
+    {
+        public string GetInputText()
+        {
+            Console.WriteLine("Enter text to store in input.txt (press ENTER on an empty line to finish):");
+
+            var lines = new List<string>();
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line))
+                    break;//empty line → finish input
+                lines.Add(line);
+            }
+
+            //join lines with Environment.NewLine to preserve new lines in file
+            return string.Join(Environment.NewLine, lines);
+        }
+        public string[] GetInputTextLines()
+        {
+            Console.WriteLine("Enter text to store in input.txt (press ENTER on an empty line to finish):");
+
+            var lines = new List<string>();//empty list of strings,we keep adding lines one by one
+            while (true)
+            {
+                string line = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(line)) break;//finish input
+                lines.Add(line);
+            }
+            return lines.ToArray();//return as array of lines
+        }
+    }
+}
